901cb56e6d07685305451aa4ddde51ac0d8acbc8
arthurcant <arthurbig19@gmail.com>
2021-08-30 20:24:12 -0300

    #7 Deleted all files and folders and Update the file CalcController.

--- script/controller/CalcController.js
+++ script/controller/CalcController.js
@@ -1,6 +1,7 @@
 class CalcController {
 
-    constructor() {        
+    constructor() {    
+        this._operation = [];    
         this._displayCalcEl = document.querySelector("#display_calc");
         this._hoursEl = document.querySelector("#hours")
         this._dateEl = document.querySelector("#date")
@@ -32,13 +33,132 @@ class CalcController {
 
     }
     
+
+    clearAll() {
+
+        this._operation = [];
+
+    }
+
+    clearEntry() {
+
+        this._operation.pop();
+
+    }
+
+    getLastOperation() {
+
+        return this._operation[this._operation.length - 1];
+
+    }
+
+    setLastOperation(value) {
+
+        this._operation[this._operation.length - 1] = value;
+
+    }
+
+    isOperator(value) {
+
+        return (['+', '-', '*', '%', '/'].indexOf(value) > -1);
+
+    }
+
+    addOperation(value) {
+
+        if (isNaN(this.getLastOperation())) {
+
+            if (this.isOperator(value)) {
+
+                this.setLastOperation(value);
+
+            } else if(isNaN(value)) {
+
+                //Outra coisa
+                console.log('value');
+
+            } else {
+
+                this._operation.push(value);
+
+            }
+
+        } else {
+
+            let newValue = this.getLastOperation().toString() + value.toString();
+            this.setLastOperation(parseInt(newValue));
+        }
+
+        console.log(this._operation);
+
+    }
+
+    setError() {
+
+        this.displayCalc = "Error";
+
+    }
+
+
+    execBtn(value) {
+
+        switch (value) {
+            case 'ac':
+                this.clearAll();
+                break;
+            case 'ce':
+                this.clearEntry();
+                break;
+            case 'plus':
+                this.addOperation('+');
+                break;
+            case 'subtract':
+                this.addOperation('-');
+                break;
+            case 'division':
+                this.addOperation('/');
+                break;
+            case 'multiplication':
+                this.addOperation('*');
+                break;
+            case 'percent':
+                this.addOperation('%');
+                break;
+            case 'equal':
+
+            break;
+            case 'ponto':
+                this.addOperation('.');
+                break;
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                this.addOperation(parseInt(value));
+                break;
+            default:
+                this.setError();
+            
+        }
+
+    }
+
+
     initButtonsEvents() {        
         let buttons = document.querySelectorAll("#buttons div");
 
         buttons.forEach((btn, index) => {
             this.addEventListenerAll(btn, 'click drag', e =>{
 
-                console.log(btn.className);
+                let textBtn = btn.className.replace("btn-","");
+
+                this.execBtn(textBtn);
 
             });
 
