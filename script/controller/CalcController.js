cd0a662b0ee71cb96819f049dea383b86b6bfe2d
arthurcant <arthurbig19@gmail.com>
2021-09-09 21:00:41 -0300

    #15 Update the file CalcController.js.

--- script/controller/CalcController.js
+++ script/controller/CalcController.js
@@ -1,7 +1,9 @@
 class CalcController {
 
     constructor() {    
-
+        
+        this._audio = new Audio('click.mp3');
+        this._audioOnOff = false;
         this._lastOperator = '';
         this._lastNumber = '';
 
@@ -31,7 +33,22 @@ class CalcController {
         this.setLastNumberToDisplay();
         this.pasteFromClipboard();
 
+        document.querySelectorAll('.btn-ac').forEach(btn => {
+            btn.addEventListener('dblclick', () => {
+                this.toggleAudio();
+            })
+        });
+    }
 
+    toggleAudio(){
+        this._audioOnOff = !this._audioOnOff;
+    }
+
+    playAudio() {
+        if (this._audioOnOff){
+            this._audio.currentTime = 0
+            this._audio.play();
+        }
     }
 
     copyToClipboard(){
@@ -66,6 +83,8 @@ class CalcController {
     initKeyboard(){
         
         document.addEventListener('keyup', e =>{
+
+            this.playAudio();
 
             switch (e.key) {
                 case 'Escape':
@@ -290,6 +309,8 @@ class CalcController {
 
 
     execBtn(value) {
+
+        this.playAudio();
 
         switch (value) {
             case 'ac':
